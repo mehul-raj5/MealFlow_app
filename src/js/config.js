72257ce78577d5eb4c5153f2fbddd96363f2937f
Mehul Raj <mehulraj@Mehuls-MacBook-Pro.local>
2025-06-24 20:17:53 +0530

    could not fix the old repo

---
+++ src/js/config.js
@@ -0,0 +1,8 @@
+// Configuration file that contains all global variables that defines important Data
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9fd0d5b5-d5ab-43da-b067-9dbd431319d0';
+// Time to Automatically-close the form window
+export const MODAL_CLOSE_SEC = 2;
